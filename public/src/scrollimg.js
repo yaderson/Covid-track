d97a21c42d750f3bcda82cfc64fc8206f8497830
Yader <yadersonjesuspatinovidal@gmail.com>
2020-05-11 20:13:01 -0500

    lint/remove unused files

--- public/src/scrollimg.js
+++
@@ -1,4 +0,0 @@
-window.onload = () => {
-    $(".g-scrolling-carousel .cards").gScrollingCarousel();
-    console.log($(".g-scrolling-carousel .cards"))
-};
--- src/assets/src/scrollimg.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    $(".g-scrolling-carousel .cards").gScrollingCarousel();
-});
